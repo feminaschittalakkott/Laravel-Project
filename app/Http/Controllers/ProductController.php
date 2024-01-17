30d85481b9844ae765cb230b94837fa2f1bcfd3e
feminaschittalakkott <feminasc442@gmail.com>
2024-01-17 23:48:37 +0400

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index(Request $request) {
+        
+        $keyword = $request->get('search');
+        $perPage = 5;
+
+        if(!empty($keyword)) {
+            $products = Product::where('name', 'LIKE', "%$keyword%")
+                        ->orWhere('category', 'LIKE', "%$keyword%")
+                        ->latest()->paginate($perPage);
+        } else {
+            $products = Product::latest()->paginate($perPage);
+        }
+        return view('products.index', ['products' => $products])->with('1', (request()->input('page', 1) -1) *5);
+    }
+
+    public function create() {
+        return view('products.create');
+    }
+
+    public function store(Request $request) {
+
+        $request->validate([
+            'name' => 'required',
+            'image' => 'required|image|mimes:jpg, png, jpeg, gif, svg|max:2028'
+        ]);
+
+        $product = new Product;
+
+        $file_name = time() . '.' . request()->image->getClientOriginalExtension();
+        request()->image->move(public_path('image'), $file_name);
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->image = $file_name;
+        $product->category = $request->category;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+
+        $product->save();
+        return redirect()->route("products.index")->with('success', 'Product Added Successfully');
+    }
+}
